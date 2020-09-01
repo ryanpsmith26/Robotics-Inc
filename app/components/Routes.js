6df9b2445660ba7219979dd82bcab9096f332e99
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-01 09:41:53 -0400

    adds nav bar with links and route to render All Robots

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AllRobots from './AllRobots';
 
 const Routes = () => {
 	return (
 		<Router>
 			<div>
-				<nav>Welcome!</nav>
+				<nav className="NavList">
+					<Link to="/">Home</Link>
+					<Link to="/robots">Robots</Link>
+					<Link to="/projects">Projects</Link>
+				</nav>
 				<main>
-					{/* <h1>
-            Welcome to StackBot Project Management: your robot employees are
-            awaiting assignments!
-          </h1>
-          <p>This seems like a nice place to get started with some Routes!</p> */}
-					<AllRobots />
+					<Route exact path="/robots" component={AllRobots} />
 				</main>
 			</div>
 		</Router>
