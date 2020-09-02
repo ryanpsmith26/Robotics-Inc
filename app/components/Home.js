6128b32a02d75f67e7f572ebfaf8b363e8dab883
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-02 10:28:28 -0400

    adds welcome image

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Home = () => <h1>Welcome to Stackbot Inc.!</h1>;
+const Home = () => {
+	return (
+		<React.Fragment>
+			<h1>Welcome to Stackbot Inc.!</h1>
+			<img src="https://compote.slate.com/images/5578848e-06d9-4d4d-a771-648c3e9dc488.jpeg?width=780&height=520&rect=1560x1040&offset=0x0" />
+		</React.Fragment>
+	);
+};
 
 export default Home;
