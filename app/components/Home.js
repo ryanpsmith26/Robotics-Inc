45ff200be5d8c8082f6f45b2591a5e50b556ebbe
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-29 15:50:33 -0400

    edits company name in Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<React.Fragment>
-			<h1 className="HomeHeader">Welcome to Stackbot Inc.!</h1>
+			<h1 className="HomeHeader">Welcome to Robotics Inc.!</h1>
 			<img src="https://compote.slate.com/images/5578848e-06d9-4d4d-a771-648c3e9dc488.jpeg?width=780&height=520&rect=1560x1040&offset=0x0" />
 		</React.Fragment>
 	);
