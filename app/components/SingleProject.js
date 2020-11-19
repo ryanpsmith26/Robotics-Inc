b116a9e1e83a0ca918e05e6bb49fed1c34ea8d62
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-11-19 14:00:40 -0500

    changes toggle status button text in single project

--- app/components/SingleProject.js
+++ app/components/SingleProject.js
@@ -62,7 +62,7 @@ export class SingleProject extends React.Component {
 								className="ToggleProjectStatusBtn"
 								onClick={() => toggleStatus(project.id)}
 							>
-								{project.completed ? 'Mark Active' : 'Mark Complete'}
+								{project.completed ? 'Active' : 'Complete'}
 							</button>
 						</div>
 					</div>
